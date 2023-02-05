e5b67bdfdff400485a2e7820851536bc97f30dc3
Lionello Lunesu <lio+git@lunesu.com>
2023-02-05 11:52:43 -0800

    multiple peers

--- index.ts
+++ index.ts
@@ -1,10 +1,20 @@
 import * as pulumi from "@pulumi/pulumi";
 import * as aws from "@pulumi/aws";
 import * as awsx from "@pulumi/awsx";
+import assert = require("assert");
 
 const config = new pulumi.Config();
 export const wgPort = config.getNumber("wgPort") || 51820; // TODO: randomize
 export const sshCidr = config.require("sshCidr");
+
+interface Peer {
+  publicKey: string;
+  allowedIPs: string; // TODO: make this an array
+  presharedKey?: string;
+}
+
+const peers = config.requireObject<Peer[]>("peers") || [];
+assert(peers.length > 0, "At least one peer is required");
 
 // Find the latest Amazon Linux 2 AMI for x64, EBS-backed instances.
 const ami = aws.ec2.getAmiOutput({
@@ -38,11 +48,21 @@ export const imageId = ami.id;
 
 const sg = new aws.ec2.SecurityGroup("secgroup", {
   ingress: [
-    { protocol: "tcp", fromPort: 22, toPort: 22, cidrBlocks: [sshCidr] },
+    { protocol: "tcp", fromPort: 22, toPort: 22, cidrBlocks: [sshCidr] }, // TODO: only when SSH is enabled
     {
       protocol: "udp",
       fromPort: wgPort,
       toPort: wgPort,
+      cidrBlocks: ["0.0.0.0/0"],
+    },
+    // By default, security groups do not allow any inbound ICMP traffic.
+    // If you don't explicitly configure an ICMP inbound rule for your security group, PMTUD is blocked.
+    // https://docs.aws.amazon.com/AWSEC2/latest/UserGuide/network_mtu.html
+    // Allow ICMP Path MTU Discovery (RFC 1213) FIXME: narrow this down
+    {
+      protocol: "icmp",
+      fromPort: -1,
+      toPort: -1,
       cidrBlocks: ["0.0.0.0/0"],
     },
   ],
@@ -51,15 +71,22 @@ const sg = new aws.ec2.SecurityGroup("secgroup", {
   ],
 });
 
+// TODO: make this optional
 const keyPair = new aws.ec2.KeyPair("keyPair", {
-  publicKey: config.require("publicKey"),
+  publicKey: config.require("sshPublicKey"),
 });
 
-// const wgCidr = config.require("wgCidr");
-const ifAddress = config.require("ifAddress"); // TODO: get from `wgCidr`
-const peerPublicKey = config.require("peerPublicKey");
-const peerAllowedIPs = config.require("peerAllowedIPs");
-const peerPresharedKey = config.get("peerPresharedKey");
+const ifAddress = config.require("ifAddress");
+
+function peerConfig(peer: Peer): string {
+  assert(peer.publicKey, "Peer must have a public key");
+  assert(peer.allowedIPs, "Peer must have allowed IPs");
+  return `[Peer]
+PublicKey = ${peer.publicKey}
+AllowedIPs = ${peer.allowedIPs}
+${peer.presharedKey ? `PresharedKey = ${peer.presharedKey}` : ""}
+`;
+}
 
 // Cloud-init script
 // NOTE: this only runs once during first boot
@@ -78,13 +105,9 @@ write_files:
       ListenPort = ${wgPort}
       PrivateKey = PRIVATEKEY
       Address = ${ifAddress}
-      SaveConfig = true
-      PostUp = iptables -A FORWARD -i %i -j ACCEPT; iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
-      PreDown = iptables -D FORWARD -i %i -j ACCEPT; iptables -t nat -D POSTROUTING -o eth0 -j MASQUERADE
-      [Peer]
-      PublicKey = ${peerPublicKey}
-      AllowedIPs = ${peerAllowedIPs}
-      ${peerPresharedKey ? `PresharedKey = ${peerPresharedKey}` : ""}
+      PostUp = iptables -A FORWARD -i eth0 -o %i -m state --state RELATED,ESTABLISHED -j ACCEPT ; iptables -A FORWARD -i %i -o eth0 -j ACCEPT ; iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
+      PostDown = iptables -D FORWARD -i eth0 -o %i -m state --state RELATED,ESTABLISHED -j ACCEPT ; iptables -D FORWARD -i %i -o eth0 -j ACCEPT ; iptables -t nat -D POSTROUTING -o eth0 -j MASQUERADE
+      ${peers.flatMap((peer) => peerConfig(peer).split('\n')).join("\n      ")}
 
 runcmd:
   - amazon-linux-extras install -y epel
@@ -110,3 +133,4 @@ const spotInstance = new aws.ec2.SpotInstanceRequest("spotInstance", {
 export const publicDns = spotInstance.publicDns;
 export const publicIp = spotInstance.publicIp;
 export const endpoint = pulumi.interpolate`${publicIp}:${wgPort}`;
+export const sshUser = pulumi.interpolate`ec2-user@${publicIp}`;
