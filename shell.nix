6f8aa28d96d30f29a93a8fed415144da68009e7d
Lionello Lunesu <lio+git@lunesu.com>
2026-06-18 13:12:35 -0700

    update nixpkgs/pulumi

--- shell.nix
+++ shell.nix
@@ -1,26 +1,14 @@
 { pkgs ? import ./nix/nixpkgs.nix { } }:
 with pkgs;
-let
-  pulumi-bin' =
-    let
-      data = import ./nix/pulumi-bin-data.nix { };
-    in
-    pulumi-bin.overrideAttrs (finalAttrs: previousAttrs: {
-      version = data.version;
-      srcs = map (x: fetchurl x) data.pulumiPkgs.${stdenv.hostPlatform.system};
-      meta.platforms = builtins.attrNames data.pulumiPkgs;
-    });
-in
-# Ensure the overridden version of Pulumi is not older than the version in the pinned Nixpkgs.
-assert -1 != builtins.compareVersions pulumi-bin'.version pulumi-bin.version;
 mkShell {
   buildInputs = [
     awscli2
     gnumake
     # gnused # force Linux `sed` everywhere
     nixfmt
-    nodejs-16_x # for Pulumi
-    pulumi-bin'
+    nodejs_22 # for Pulumi
+    pulumi
+    pulumiPackages.pulumi-nodejs
     # saml2aws
     wireguard-tools
   ];
